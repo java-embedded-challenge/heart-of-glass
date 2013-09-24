f4c85de529de90490d02e94638bf86fb166dbc13
Steven Lizarazo <stv-contact@hotmail.com>
2013-09-24 11:01:23 -0700

    Revert "Heart Beat Graph, Raspberri PI app and integration with the heartbeat device."
    This reverts commit 694c4ef68dc38a1b21b99cdd9b24f7d909a29b1b.

--- web/heartbeat.js
+++ web/heartbeat.js
@@ -3,181 +3,82 @@
  * and open the template in the editor.
  */
 
-var ldata = new Array();
-var ldatalie = new Array();
-var lnum = 0;
-var lw = 999;
-var hg = 50;
-var threshold=130;
-var avg = 50;
-var lie = false;
+var ldata=new Array();
+var lnum=0;
+var lw=999;
+var hg=50;
+var avg=50;
+function run(){
 
-function favg(ar){
-    
-      var sum = 0;
-            for (var i = 0; i < ar.length; i++) {
-                sum = sum + parseInt(ar[i]);
-                // Do something with element i.
-            }
-          var  avg = sum;
-            if (ar.length > 0){
-                avg = sum / ar.length;}
-    return avg;
-}
-
-
-function standardeviation(ar){
-    var av=favg(ar);
-      var sum = 0;
-            for (var i = 0; i < ar.length; i++) {
-                sum = sum + Math.pow( (parseInt(ar[i]) -av),2);
-                // Do something with element i.
-            }
-          var  sd = 0;
-            if (ar.length > 0)
-                sd = Math.sqrt( sum / ar.length);
-            //alert(sum+" "+ar.length+' '+ sd);
-    return sd;
-}
-
-
-function liedetect(ar){
-     var av=favg(ar);
-var sd= standardeviation(ar);
+            $.ajax({
+url: 'index.jsp',
+type: "GET",
+dataType: "json",
+success: function (data) {
       
-            for (var i = 0; i < ar.length; i++) {
-                if( Math.abs( parseInt(ar[i])-avg) >sd*2 ){
-                    ldatalie[i]=true;
-                    //alert('lie at'+i);
-                    lie=true;
-                }
-                // Do something with element i.
-            }
-}
-function reset(){
-     ldata = new Array();
- ldatalie = new Array();
- lie=false;
+ldata=new Array();
 
-}
-function put(e){
-  ldata.push(e);
-  ldatalie.push(false);
-  
-}
-function run() {
+  $.each(data, function(i,item){
+      ldata.push(item);
+if(item>hg)hg=item;
+if(item<lw)lw=item;
+ lnum=item;
 
-    $.ajax({
-        url: 'index.jsp',
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-reset();
-           
-            $.each(data, function(i, item) {
-put(item);
-
-if (item > hg)
-                    hg = item;
-                if (item < lw)
-                    lw = item;
-                lnum = item;
-
-            });
-           
-            avg = favg(ldata);
-
-            $("#number").html(lnum);
-            $("#avg").html(parseInt(avg));
-
-            liedetect(ldata);
-
-            draw();
-
-            $("#himage").fadeToggle('slow');
-        }
-    });
-    
-    
-   if(hg>threshold) logWarn('Too high '+hg );
-   if(lie==true) logWarn('Lie detected');
-    setTimeout(run, 1000);
-
-}
-var w = 640, h = 480, lim = 30;
-
-
-function draw() {
-
-    var drawingCanvas = document.getElementById('draw');
-    if (drawingCanvas.getContext) {
-// Initaliase a 2-dimensional drawing context
-        var context = drawingCanvas.getContext('2d');
-//Canvas commands go here
-        var i = 0;
-
-
-        context.beginPath();
-        context.fillStyle = "#F0F0F0";
-        context.fillRect(0, 0, w, h);
-        context.fillStyle = "#0a0a0a";
-        var length = ldata.length,
-                element = null;
-        context.moveTo(0, h / 1);
-
-        for (var i = 0; i < length; i++) {
-            element = ldata[i];
-            var nx=i * (w / lim);
-            var ny=h / 2 + 100 - element;
-            context.lineTo(nx, ny);
-
-if(ldatalie[i]==true) context.arc(nx, ny, 4, 0,360,false);
-     
+                         });
+var sum=0;
+for (var i = 0; i < ldata.length; i++) {
+       sum=sum+ parseInt(ldata[i]);
             // Do something with element i.
-        }
+}
+ avg=sum;
+if(ldata.length>0)avg=sum/ldata.length;
 
-        context.stroke();
+$("#number").html(lnum);
+$("#avg").html( parseInt(avg));
+
+draw();
+
+$("#himage").fadeToggle('slow');
+}
+});
+            setTimeout(run,1000);
+
+}
+var w=640,h=480,lim=30;
+
+
+function draw(){
+
+var drawingCanvas = document.getElementById('draw');
+if(drawingCanvas.getContext) {
+// Initaliase a 2-dimensional drawing context
+var context = drawingCanvas.getContext('2d');
+//Canvas commands go here
+var i=0;
+
+
+context.beginPath();
+context.fillStyle="#F0F0F0";
+context.fillRect(0,0,w,h);
+context.fillStyle="#0a0a0a";
+var length = ldata.length,
+    element = null;
+    context.moveTo(0, h/1);
+    
+for (var i = 0; i < length; i++) {
+       element = ldata[i];
+     context.lineTo(i*(w/lim),h/2+100-element);
+
+            // Do something with element i.
+}
+        
+context.stroke();
 //context.fillStyle = "#fafafa";
 //context.moveTo(0, h/2+100-avg);
 //context.lineTo(w,h/2+100-avg);
 //context.stroke();
 //context.endPath();
 
-    }
-
 }
 
-
-function countChildElements(parent, child)
- {
- try{var parent = document.getElementById(parent);
- var childCount = parent.getElementsByTagName(child).length;
- return childCount;
- }catch(e){return 0;}
- }
-
-var c=0;
-
-function delFirst(){
-
- if(countChildElements("log",'p')>4){
- document.getElementById("log").removeChild(document.getElementById("log").firstChild);
- }
 }
-
-
-
-function logWarn(s){
-//alert(s);
-delFirst();
-    $("#log").append('<p class = "warn">'+s+'</p>');
-}
-
-
-function log(s){
-//alert(s);
-delFirst();
-    $("#log").append('<p>'+s+'</p>');
-}
-
-log('Welcome');
