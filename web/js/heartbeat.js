3c824ed12292e635176db1331a6db19174ef6d23
Steven Lizarazo <stv-contact@hotmail.com>
2013-09-25 09:53:22 -0700

    New  names for variables, some bugs fixed, commented code

--- web/js/heartbeat.js
+++ web/js/heartbeat.js
@@ -2,13 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-var ldata = new Array();
-var lnum = 80;
-var maxhb = 240;
-var threshold = 180;
-var lthreshold = 80;
 var w = 1000, h = 550, lim = 30;
+
+var heart_beat_recent_data = new Array();
+var last_heart_beat = 80;
+var upper_heart_beat_limit = 240;
+var normal_heart_beat_limit = 180;
+var below_normal_heart_beat_limit = 80;
 
 
 function start() {
@@ -20,77 +20,88 @@ function start() {
 
 function run() {
 
-    $.ajax({
+    $.ajax({//ajax request to get new samples from the raspberry pi
         url: 'data.jsp',
         type: "GET",
         dataType: "json",
         success: function(data) {
-            
-            lnum = data[data.length - 1];
 
-         ldata=data;
+            last_heart_beat = data[data.length - 1];
 
-            $("#number").html(lnum);
+            heart_beat_recent_data = data;
+
+            $("#number").html(last_heart_beat);
             draw();
         }
     });
 
 
-//    if (hg > threshold)
-  //      logWarn('Too high ' + hg);
-   
-    setTimeout(run, 1000);
+//    if (hg > normal_heart_beat_limit)
+    //      logWarn('Too high ' + hg);
+
+    setTimeout(run, 1000);//get new samples every second
 
 }
 
+
+function secureHeartBeat(b){//check heart beat valuest to avoid animation bug
+    if(b<1)return 10;
+    if(b>250)return 250;
+    return b;
+    
+}
 function beatOn() {
-if(lnum<lthreshold){
-    $("#himage").attr("src", "images/heart.png");
-}else
-if(lnum<threshold){
-    $("#himage").attr("src", "images/heartg.png");
-}else
-{
-    $("#himage").attr("src", "images/heartr.png");
+    
+    //chenge the color of the heart according to the limits
+    if (last_heart_beat < below_normal_heart_beat_limit) {
+        $("#himage").attr("src", "images/heart.png");
+    } else
+    if (last_heart_beat < normal_heart_beat_limit) {
+        $("#himage").attr("src", "images/heartg.png");
+    } else
+    {
+        $("#himage").attr("src", "images/heartr.png");
 
-}
+    }
 
-    $("#himage").fadeIn({duration: 50});
-    setTimeout(beatOff, 60000 / lnum / 2);
+    $("#himage").fadeIn({duration: 50});//shows the heart
+    // 60000 / last_heart_beat / 2= half time of a heartbeat for fadeif
+    setTimeout(beatOff, 60000 / secureHeartBeat(last_heart_beat) / 2);
 }
 
 function beatOff() {
-    $("#himage").fadeOut({duration: 50});
-    setTimeout(beatOn, 60000 / lnum / 2);
+    $("#himage").fadeOut({duration: 50});//hides the heart
+       // 60000 / last_heart_beat / 2= half time of a heartbeat for fadeOut
+    setTimeout(beatOn, 60000 / secureHeartBeat(last_heart_beat) / 2);
 
 }
 
 
 function draw() {
-
-    var drawingCanvas = document.getElementById('draw');
+//This is for drawing all the lines
+    var drawingCanvas = document.getElementById("draw");
     if (drawingCanvas.getContext) {
         var context = drawingCanvas.getContext('2d');
         var i = 0;
 
-
+//draw background zones black green red
         context.beginPath();
         context.save();
         context.fillStyle = "#660900";
-        context.fillRect(0, 0, w, h - (threshold * h / maxhb));
+        context.fillRect(0, 0, w, h - (normal_heart_beat_limit * h / upper_heart_beat_limit));
         context.fill();
 
         context.restore();
         context.save();
         context.fillStyle = "#5b8b02";
-        context.fillRect(0, h - (threshold * h / maxhb), w, h - (lthreshold * h / maxhb));
+        context.fillRect(0, h - (normal_heart_beat_limit * h / upper_heart_beat_limit), w, h - (below_normal_heart_beat_limit * h / upper_heart_beat_limit));
         context.fill();
 
         context.restore();
 
         context.save();
         context.fillStyle = "#0000";
-        context.fillRect(0, h - (lthreshold * h / maxhb), w, h);
+        context.fillRect(0, h - (below_normal_heart_beat_limit * h / upper_heart_beat_limit), w, h);
         context.fill();
 
 
@@ -100,6 +111,7 @@ function draw() {
         context.restore();
 
 
+//gradient for the line
 
 
         var gradient = context.createLinearGradient(0, h, 0, 0);
@@ -111,13 +123,15 @@ function draw() {
         gradient.addColorStop(1.0, "#ff9090");
 
         context.strokeStyle = gradient;
-        var length = ldata.length,
+        var length = heart_beat_recent_data.length,
                 element = null;
         context.lineWidth = 5;
+        //the line
+
         for (var i = 0; i < length; i++) {
-            element = ldata[i];
+            element = heart_beat_recent_data[i];
             var nx = i * (w / lim);
-            var ny = h - element * h / maxhb;
+            var ny = h - element * h / upper_heart_beat_limit;
             context.lineTo(nx, ny);
         }
 
@@ -141,6 +155,7 @@ function countChildElements(parent, child)
 
 var c = 0;
 
+//this is to remove the first child of the log console
 function delFirst() {
 
     if (countChildElements("log", 'p') > 2) {
@@ -164,7 +179,7 @@ function log(s) {
 }
 
 
-
+//this is to get age ranges 
 var ageTargetRates = {
     20: [100, 170, 200],
     25: [95, 162, 190],
@@ -179,14 +194,16 @@ var ageTargetRates = {
     70: [75, 128, 150]
 };
 
+//this is to set limits and values
 
 function setLimits() {
-    var age = window.location.hash.substring(1);
-    age = 31;
+    var age = window.location.hash.substring(1);//get the age
+    if(parseInt(age)<1||age==''||age==undefined) age = 31;
+    
     var targetRates = ageTargetRates[Math.floor((parseInt(age) + 4) / 5) * 5];
-    lthreshold = targetRates[0];
-    threshold = targetRates[1];
-    maxhb = targetRates[2];
+    below_normal_heart_beat_limit = targetRates[0];
+    normal_heart_beat_limit = targetRates[1];
+    upper_heart_beat_limit = targetRates[2];
 
     ;
 }
